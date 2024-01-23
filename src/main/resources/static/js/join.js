4d698f69c27706cda3a3469bbc2655bbb1644d23
jeein2222 <jiinbag057@gmail.com>
2024-01-23 15:41:08 +0900

    ec2 주소 변경

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -8,7 +8,7 @@ function ajax_join(frm){ //회원가입
         data:JSON.stringify(formData),
         success:function(data){
             console.log(data.data);
-            window.location.href = 'http://ec2-15-165-161-6.ap-northeast-2.compute.amazonaws.com:8080//ds-sw/auth/login';
+            window.location.href = 'http://ec2-15-165-161-6.ap-northeast-2.compute.amazonaws.com:8080/ds-sw/auth/login';
             alert("회원가입 성공!");
         },error:function(xhr,status,error){
             console.log("["+xhr.status+"]"+error);
